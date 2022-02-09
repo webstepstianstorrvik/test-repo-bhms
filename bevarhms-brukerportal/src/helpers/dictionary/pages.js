285a8c8d3b3a9e0c8102f4b9c748d81fa21ee4ba
Stian Storrvik <stianstorrvik@gmail.com>
2022-02-09 15:11:55 +0100

    Restructure

--- bevarhms-brukerportal/src/helpers/dictionary/pages.js
+++
@@ -1,5 +0,0 @@
-export const PAGE_BRUKERVEILEDNING = {
-    id: 1,
-    parenttitle: 'Veiledning',
-    title: 'Brukerveiledning'
-};
---
+++ src/helpers/dictionary/pages.js
@@ -0,0 +1,13 @@
+export const PAGE_BRUKERVEILEDNING = {
+    id: 1,
+    parenttitle: 'Veiledning',
+    title: 'Opplæringsvideoer'
+};
+
+export const PAGE_FAGVIDEOER = {
+    id: 2,
+    parenttitle: 'Veiledning',
+    title: 'Fagvideoer'
+};
+
+
