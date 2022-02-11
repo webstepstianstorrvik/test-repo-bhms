d64dfc491e9f7c241d58e2a411cc943434db418e
Stian Storrvik <stianstorrvik@gmail.com>
2022-02-11 07:46:48 +0100

    remove unused subheader import

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,7 +2,6 @@ import '../../assets/css/fonts.css';
 
 import React from 'react';
 import Topheader from './Topheader.js';
-import Subheader from './Subheader.js';
 
 class Header extends React.Component {
     render() {
