c6ccadfa0a223c7152f7d7b03af27f85ed3fac57
Joachim Eivindsen <joachim.eivindsen@webstep.no>
2022-08-09 09:12:52 +0200

    Add startside

--- src/components/startside/Startside.js
+++ src/components/startside/Startside.js
@@ -1,12 +1,68 @@
-import '../../assets/css/fonts.css';
-import React from 'react';
-class Startside extends React.Component {
+import './startside.css';
 
-    render() {
-        return (
-            <div>En annen side</div>
-        );
-    }
+import React from 'react';
+import Button from "../common/Button";
+
+import NyKontaktIcon from '../../assets/img/icons/startside/nykontakt.svg';
+import OmBevarHMSIcon from '../../assets/img/icons/startside/ombevarhms.svg';
+import StatusRapportIcon from '../../assets/img/icons/startside/statusrapport.svg';
+import TilsynFraMyndigheteneIcon from '../../assets/img/icons/startside/tilsynframyndighetene.svg';
+import Card from '../common/Card';
+import Borettslaget from './Borettslaget';
+import MineFrister from './MineFrister';
+import Nyheter from './Nyheter';
+
+
+const Startside = () => {
+
+    const buttons = [
+        {
+            link: '#',
+            icon: NyKontaktIcon,
+            title: 'Ny kontakt'
+        },
+        {
+            link: '#',
+            icon: OmBevarHMSIcon,
+            title: 'Om BevarHMS'
+        },
+        {
+            link: '#',
+            icon: StatusRapportIcon,
+            title: 'Status rapport'
+        },
+        {
+            link: '#',
+            icon: TilsynFraMyndigheteneIcon,
+            title: 'Tilsyn fra myndighetene'
+        },
+    ]
+    
+    return (
+        <div className="startside grid">
+            <div className="borettslaget-wrapper">
+                <Card>
+                    <Borettslaget />
+                </Card>
+            </div>
+            <div className="mine-frister-wrapper">
+                <Card title="Mine frister">
+                    <MineFrister />
+                </Card>
+            </div>
+            <div className="nyheter-wrapper">
+                <Card title="Nyheter">
+                   <Nyheter />
+                </Card>
+            </div>
+            <div className="aktuelle-tema-wrapper">
+                <h2>Aktuelle tema</h2>
+                <div className="container grid">
+                    {buttons.map(({title, icon, link}) => <Button key={title} title={title} icon={icon} link={link} />)}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Startside;
