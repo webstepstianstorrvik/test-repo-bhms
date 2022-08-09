83d0466bafc01ee49b89e543be5178cfc2077f75
Joachim Eivindsen <joachim.eivindsen@webstep.no>
2022-08-09 17:10:44 +0200

    Add calender component for activities page

--- src/components/aktiviteter/mine-frister/MineFrister.js
+++ src/components/aktiviteter/mine-frister/MineFrister.js
@@ -1,10 +1,38 @@
-import React from "react";
+import "./minefrister.css";
+
+import React, { useState, useMemo } from "react";
+import Calendar from "../../common/Calendar";
+
+import CalendarButtonIcon from "../../../assets/img/icons/handbok/skjema.svg"
+import ListButtonIcon from "../../../assets/img/icons/aktiviteter/oversikt.svg"
+import PagesButtonIcon from "../../../assets/img/icons/arkiv/arkiv.svg"
+import Table from "../../common/Table";
+
 
 const MineFrister = () => {
+    const [view, setView] = useState("CALENDAR")
+
+    const events = useMemo(() => [], [])
     
     return (
-        <div>
-        </div>
+        <>
+            <div className="top-heading-button-wrapper">
+                <div className="toggle-view-button-wrapper">
+                    <button className="btn btn-secondary toggle-view-button" onClick={() => setView("CALENDAR")}><img src={CalendarButtonIcon} alt="calender-toggle-icon"/></button>
+                    <button className="btn btn-secondary toggle-view-button" onClick={() => setView("LIST")}><img src={ListButtonIcon} alt="list-toggle-icon"/></button>
+                    <button className="btn btn-secondary toggle-view-button" onClick={() => setView("PAGES")}><img src={PagesButtonIcon} alt="pages-toggle-icon"/></button>
+                </div>
+                <div className="info-button-wrapper">
+                    <button className="btn btn-secondary info-button">Veiledning</button>
+                    <button className="btn btn-secondary info-button">Skriv ut</button>
+                </div>
+            </div>
+            
+            <div>
+                {view === "CALENDAR" && <Calendar events={events}/>}
+                {view === "LIST" && <Table events={events}/>}
+            </div>
+        </>
     )
 }
 
---
+++ src/components/common/Table.js
@@ -0,0 +1,12 @@
+import "./table.css";
+import React from "react";
+import Card from "./Card";
+
+const Table = () => {
+
+    return (
+        <Card>table</Card>
+    );
+}
+
+export default Table;
