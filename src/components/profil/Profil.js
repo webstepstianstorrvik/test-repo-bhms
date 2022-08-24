9465a3fcafb1c6b6e1149bd824de57139e54cb7b
Joachim Eivindsen <joachim.eivindsen@webstep.no>
2022-08-24 14:09:23 +0200

    Remove auth content from profil route

--- src/components/profil/Profil.js
+++ src/components/profil/Profil.js
@@ -1,17 +1,9 @@
 import React from "react";
 
-import { useAuth } from 'oidc-react';
-
 const Profil = () => {
-    const auth = useAuth();
-    const { name, phone_number, metadata_email } = auth.userData.profile
 
     return (
         <div>
-            <p><b>Følgende info er hentet fra auth:</b></p>
-            <p>{name}</p>
-            <p>{phone_number}</p>
-            <p>{metadata_email}</p>
         </div>
     )
 }
