60626dc07ed409b28a3d11e100de0892a5bdfa66
Joachim Eivindsen <joachim.eivindsen@webstep.no>
2022-10-14 08:37:07 +0200

    bump react-table and update Table component to tsx

--- src/components/aktiviteter/mine-frister/MineFristerListView.tsx
+++ src/components/aktiviteter/mine-frister/MineFristerListView.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import Table from '../../common/Table'
 import { Aktivitet } from './MineFrister'
 
+import { createColumnHelper } from '@tanstack/react-table'
+
 interface IMineFristerListViewProps {
     events: Aktivitet[]
 }
@@ -9,31 +11,30 @@ interface IMineFristerListViewProps {
 const MineFristerListView = ({ events }: IMineFristerListViewProps) => {
     const [filter, setFilter] = useState('Aktive')
 
-    const columns = React.useMemo(
-        () => [
-            {
-                header: 'Tittel',
-                accessor: 'tittel',
-            },
-            {
-                header: 'Hyppighet',
-                accessor: 'hyppighet',
-            },
-            {
-                header: 'Neste frist',
-                accessor: 'nesteFrist',
-            },
-            {
-                header: 'Sist utført',
-                accessor: 'sistUtført',
-            },
-            {
-                header: 'Ansvarlige',
-                accessor: 'ansvarlige',
-            },
-        ],
-        []
-    )
+    const columnHelper = createColumnHelper<Aktivitet>()
+
+    const columns = [
+        columnHelper.accessor('tittel', {
+            cell: (info) => info.getValue(),
+            header: () => <span>Tittel</span>,
+        }),
+        columnHelper.accessor('hyppighet', {
+            cell: (info) => info.getValue(),
+            header: () => <span>Hyppighet</span>,
+        }),
+        columnHelper.accessor('nesteFrist', {
+            cell: (info) => info.getValue(),
+            header: () => <span>Neste frist</span>,
+        }),
+        columnHelper.accessor('sistUtført', {
+            cell: (info) => info.getValue(),
+            header: () => <span>Sist utført</span>,
+        }),
+        columnHelper.accessor('ansvarlige', {
+            cell: (info) => info.getValue(),
+            header: () => <span>Ansvarlige</span>,
+        }),
+    ]
 
     const filteredData = events.filter((row) =>
         filter === 'Aktive' ? row.isActive : !row.isActive
